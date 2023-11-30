9056ea5765493a14e7d8a3d1779f4c023be92bf3
Paulo Peter <eduardopeter91@gmail.com>
2023-11-30 10:16:51 -0300

    Migrate/pipDecouple/dj-database/black/Create .env

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)  # TITULO
+    created_at = models.DateTimeField(
+        auto_now_add=True, null=False, blank=False
+    )  # DATA DE CRIAÇÃO
+    deadline = models.DateTimeField(null=False, blank=False)  # DATA DE ENTREGA
+    finished_at = models.DateTimeField(null=True, blank=False)  # DATA DE FINALIZAÇÃO
